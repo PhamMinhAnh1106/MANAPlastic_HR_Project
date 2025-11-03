03de51181497a7ebe533035ca3113a0a098bd371
NgocAn3105 <ngocancd2020@gmail.com>
2025-11-04 05:36:01 +0700

    update chuc nang refreshtoken

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { refreshAccessToken } from "../utils/token.utils";
 const url = "http://localhost:8080";
 export const api = axios.create({
     baseURL: url,
@@ -13,6 +14,33 @@ api.interceptors.request.use((config) => {
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 })
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        if (error.response) {
+            if (error.response.status === 403) {
+                console.warn("Access token đã hết hạn hoặc không hợp lệ.");
+
+
+
+                // Hoặc nếu bạn có refresh token API, có thể gọi ở đây
+                const refreshtoken = getCookie('refreshToken');
+                if (refreshtoken != null) {
+
+                    const res = await refreshAccessToken(refreshtoken) as { token: string, refreshToken: string };
+                    document.cookie = `access_token=${res.token}; path=/;`
+                    document.cookie = `refreshToken=${res.refreshToken}; path=/;`
+
+                }
+            }
+        }
+
+        // Nếu là lỗi khác, vẫn trả về để xử lý tiếp
+        return Promise.reject(error);
+    }
+);
 
 function getCookie(name: string): string | null {
     const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
--- frontend/src/app/utils/token.utils.ts
+++ frontend/src/app/utils/token.utils.ts
@@ -1,4 +1,5 @@
 import { jwtDecode } from 'jwt-decode';
+import { api } from '../services/api.service';
 
 interface token_res {
     "roles": string[]
@@ -8,4 +9,8 @@ interface token_res {
 export function DecodeTokenRole(token: string) {
     const payload = jwtDecode<token_res>(token);
     return payload.roles;
+}
+export async function refreshAccessToken(refreshtoken: string) {
+    const res = await api.post("/refresh", { refreshToken: refreshtoken });
+    return res.data;
 }
